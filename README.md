31becea1f819bfcf7ebeed5c28b7107341701f9d
Ali Renan Uyanık <renanuya@gmail.com>
2023-07-25 02:02:41 +0300

    Container & SizedBox

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# flutter_check
+# flutter_recheck
 
-A new Flutter project.
+In this repository, I will document everything I learn and everything I already know.
+
+## Table of Contents
+
+- [Project Description](#project-description)
+- [Resources I use](#resources-i-use)
+
+## Project Description
+
+I noticed that I have some gaps in my knowledge while working on a project with Flutter, so I decided to watch HardwareAndro's YouTube video series to fill in those gaps. I've created this repository for that purpose. In this repository, I will document everything I learn and everything I already know.
+
+## Resources I use
+- [HardwareAndro](https://www.youtube.com/@HardwareAndro)
+
---
+++ lib/101/container_sizedbox_widget_recheck.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class RecheckContainerSizedboxWidgetPages extends StatelessWidget {
+  const RecheckContainerSizedboxWidgetPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //! Ask
+            const SizedBox.shrink(),
+            //In this SizedBox we have used "SizedBox.square" to make a square box.
+            // With "dimension" we have set the size of the box. (200x200)
+            SizedBox.square(
+              dimension: 200,
+              child: Text('This is a square box. ' * 10),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const RecheckContainerSizedboxWidgetPages()));
+              },
+              child: const Text('Go to Container & SizedBox Widget Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
